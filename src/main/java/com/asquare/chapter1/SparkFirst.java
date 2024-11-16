a45f0cdc2c4a004f7e21aa99c308721854651778
afzal232 <afzal232@gmail.com>
2024-11-16 22:52:23 +0530

    Add 1st level spark dtl

---
+++ src/main/java/com/asquare/chapter1/SparkFirst.java
@@ -0,0 +1,38 @@
+package com.asquare.chapter1;
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+public class SparkFirst {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        try(var spark = SparkSession.builder().appName("SparkFirst")
+                .master("local[*]")
+                .getOrCreate();
+            final var sc = new JavaSparkContext(spark.sparkContext());){
+
+            final var list = Stream.iterate(1, n-> n+1).limit(5)
+                    .collect(Collectors.toList());
+            list.forEach(System.out::println);
+
+            final var myRdd = sc.parallelize(list);
+            System.out.printf("Total Element count in RDD::  %d%n", myRdd.count());
+            System.out.printf("Total No of partition in RDD::  %d%n", myRdd.getNumPartitions());
+
+            final var rddMin= myRdd.reduce(Integer::min);
+            final var rddMax= myRdd.reduce(Integer::max);
+            final var rddSum= myRdd.reduce(Integer::sum);
+
+            System.out.printf("Min %d , Max %d , Sum %d ", rddMin, rddMax, rddSum);
+
+        }
+
+    }
+}
